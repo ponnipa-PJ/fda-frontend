76e1f8d68ebd8c27e8e3eb0c989922cd85a2bd0b
ponnipa <33538593+ponnipa-PJ@users.noreply.github.com>
2023-11-23 19:36:10 +0700

    update

--- src/services/LinkService.js
+++ src/services/LinkService.js
@@ -2,7 +2,8 @@
 class LinkService {
   getpythonlink() {
     // return 'https://api-fda.ponnipa.in.th';
-    return 'http://localhost:8081';
+    return 'https://api-fda.ci2ict.com'
+    // return 'http://localhost:8081';
   }
 }
 
